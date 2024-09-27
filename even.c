54f11ece182776a2544496c1e4921ea04259054c
Akash baghel <akashbaghel716@gmail.com>
2024-09-27 16:33:38 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <conio.h>
+void main(){
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    
+
+if (a%2==0)
+{
+    printf("number is even");
+
+}else{
+    printf("number is odd");
+}
+
+}

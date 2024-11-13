4cfb521007b8a1d2550e0cdf2f8ae519db2d923a
Akash baghel <akashbaghel716@gmail.com>
2024-11-13 14:27:24 +0530

    Add files via upload

---
+++ akash 12.cpp
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ int main(){
+ 	
+ int i,j,m,n;
+ 	printf("enter  row and columns");
+ 	scanf("%d %d",&m,&n);
+ 	for(i=0;i<m;i++){
+ 	for(j=0;j<n;j++){
+ 		scanf("%d,&a[i][j]");
+ 		printf("%d",a[i][j]);
+}
+}
+   return 0;
+}
